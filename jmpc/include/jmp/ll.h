55dfc75fd1c3c87111ebc27a68f194a731567f90
ReT-86 <remy.n.todd@gmail.com>
2024-07-11 17:05:13 -0700

    more linked list functions!

--- jmpc/include/jmp/ll.h
+++ jmpc/include/jmp/ll.h
@@ -7,9 +7,15 @@ typedef struct ll_node {
   void *data;
 } llnode;
 
+#define ll_data(node, type) (type)node->data
+#define ll_read(root, idx, type) (type)ll_get(root, idx)->data
+
 llnode *ll_new(void *data);
 void ll_destroy(llnode *root);
 
+void ll_insert(llnode *root, void *data);
+llnode *ll_get(llnode *root, sz idx);
 llnode *ll_end(llnode *root);
+sz ll_len(llnode *root);
 
 #endif // __JMPC_LL_H
--- jmpc/src/impl/ll.c
+++ jmpc/src/impl/ll.c
@@ -21,12 +21,42 @@ void ll_destroy(llnode *root) {
   free(node);
 }
 
-llnode *ll_end(llnode *root) {
+void ll_insert(llnode *root, void *data) {
+  llnode *node = ll_new(data);
+  
+  llnode *end = ll_end(root);
+  
+  end->next = node;
+  node->prev = end;
+}
+
+llnode *ll_get(llnode *root, sz idx) {
   llnode *node = root;
+  sz i = 0;
+  
+  while (node->next && i++ <= idx)
+    node = node->next;
+  
+  return node;
+}
+
+sz ll_len(llnode *root) {
+  llnode *node = root;
+  sz size = 1;
   
   while (node->next) {
     node = node->next;
+    size++;
   }
+  
+  return size;
+}
+
+llnode *ll_end(llnode *root) {
+  llnode *node = root;
+  
+  while (node->next)
+    node = node->next;
   
   return node;
 }
